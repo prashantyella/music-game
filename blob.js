43f3d6260649d2d45256e579bee16c8de8b85957
matipina <mlpina@uc.cl>
2023-11-03 02:58:52 -0400

    Implement GUI and character creation structure

--- blob.js
+++ blob.js
@@ -3,8 +3,6 @@ class Blob {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.xCoordinates = [];
-    this.yCoordinates = [];
     this.angryLevel = 0;
     this.maxNoise = 2 + this.angryLevel * 9;
     this.phase = 0;
@@ -12,35 +10,50 @@ class Blob {
     this.offset = 0.01;
     this.zoff = 0;
     this.zoffRate = 0.01;
-    this.blobiness;
+    this.blobiness = 20;
+    this.xCoordinates = [];
+    this.yCoordinates = [];
+    this.layers = 1;
+  }
+
+  addLayer() {
+
   }
 
   draw() {
+    push();
     translate(this.x, this.y);
-    fill(222, 239, 231, 100);
-    beginShape();
-    this.maxNoise = 2 + this.angryLevel * 9;
+    fill(222, 239, 231, 15);
+    stroke(255);
+    strokeWeight(1);
+
+    this.maxNoise = 2 + this.angryLevel * 6;
     this.phaseRate = 0.007 + this.angryLevel * 0.007;
     this.zoffRate = 0.01 + this.angryLevel * 0.01;
-    this.blobiness = 30 + this.angryLevel * 20;
+    this.blobiness = 20 + this.angryLevel * 20;
     this.offset = 0.01 + this.angryLevel * 10;
 
     let xoff;
     let yoff;
-    for (var a = 0; a < TWO_PI; a += 0.1) {
-      xoff = map(cos(a + this.phase), -1, 1, 0, this.maxNoise);
-      yoff = map(sin(a + this.phase), -1, 1, 0, this.maxNoise);
-      this.offset = map(noise(xoff, yoff, this.zoff), 0, 1, -this.blobiness, this.blobiness);
 
-      let x = (this.radius + this.offset) * cos(a);
-      let y = (this.radius + this.offset) * sin(a);
-      vertex(x, y);
-      this.xCoordinates.push(x);
-      this.yCoordinates.push(y);
+    for (let l = 1; l <= this.layers; l++) {
+      beginShape();
+      for (var a = 0; a < TWO_PI; a += 0.1) {
+        xoff = map(cos(a + this.phase), -1, 1, 0, this.maxNoise);
+        yoff = map(sin(a + this.phase), -1, 1, 0, this.maxNoise);
+        this.offset = map(noise(xoff, yoff, this.zoff), 0, 1, -this.blobiness, this.blobiness);
+  
+        let x = (this.radius * l + this.offset) * cos(a);
+        let y = (this.radius * l + this.offset) * sin(a);
+        vertex(x, y);
+        this.xCoordinates.push(x);
+        this.yCoordinates.push(y);
+      }
+      endShape(CLOSE);
     }
-    endShape(CLOSE);
 
     this.phase += this.phaseRate;
     this.zoff += this.zoffRate;
+    pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,67 @@
 // Inspired in Daniel Shiffman's code:
 // https://www.youtube.com/watch?v=ZI1dmHv3MeM&ab_channel=TheCodingTrain
 
+// Create a variable for the gui context that we can use to change style
+let gui;
 
 let blob;
+let rSlider;
+let blolbSlider;
+let noiseSlider;
+let layerSlider;
+let skipButton;
+let sliderW = 200;
+let sliderH = 32;
+let buttonW = 100;
+let buttonH = 40;
+let lvl = 1;
+let attributes = 5;
+let scene = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  blob = new Blob(width/2, height/2, 60);
+  gui = createGui();
+
+  rSlider = createSlider('Radius', windowWidth - sliderW - 20, 20, sliderW, sliderH, 10, 80);
+  rSlider.val = 20;
+  layerSlider = createSlider('Layers', windowWidth - sliderW - 20, 20 + sliderH, sliderW, sliderH, 1, 30);
+  layerSlider.val = 1;
+
+  skipButton = createButton('Next', windowWidth - buttonW - 40, windowHeight - 40 - buttonH, buttonW, buttonH);
+
+
+  blob = new Blob(width/2, height/2 - 60, 30);
 }
 
 function draw() {
-  background(95, 205, 217);
+  background(0);
+  stroke(255);
+  fill(255, 70);
+  textSize(34);
+
+  blob.radius = rSlider.val;
+  blob.layers = layerSlider.val;
   blob.draw();
+
+
+  if (scene == 0) {
+    rect(20, windowHeight - 240, windowWidth - 40, 220, 10);
+    fill(255);
+    text('test test test test', 40, windowHeight - 200)
+    drawGui();
+
+  }
+
+  if (skipButton.isPressed) {
+    if (lvl < attributes) {
+      lvl += 1;
+      console.log('lvl: ', lvl);
+      layerSlider.val = lvl;
+    } else {
+      scene = 1;
+      blob.y += 60;
+      blob.r += 10;
+    }
+  }
+
 }
