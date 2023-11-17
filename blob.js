4f137bf66f3ee5d985acb664a885b91f1a754f17
matipina <mlpina@uc.cl>
2023-11-16 23:39:18 -0500

    Implemented goals and lights

--- blob.js
+++ blob.js
@@ -19,16 +19,17 @@ class Blob {
     this.offset = 0.01;
     this.zoff = 0;
     this.zoffRate = 0.01;
-    this.blobiness = 20;
+    this.blobiness = 10;
     this.layers = 1;
     this.ringDistance = 40;
     this.rings = [];
     this.paths = [];
-    this.player = new Player(9);
+    this.player = new Player(12);
     this.xCount = 0;
     this.yCount = 0;
     this.keyIndex = 0;
     this.currentRing = 0;
+    this.goals = [];
   }
 
   addPaths(n) {
@@ -54,7 +55,14 @@ class Blob {
 
   addLayer(radius) {
     let newLayer;
-    newLayer = new Ring(this.layers, radius, this.phase, this.zoff);
+    newLayer = new Ring(
+      this.layers,
+      radius,
+      this.phase,
+      this.zoff,
+      this.blobiness,
+      this.maxNoise
+    );
     newLayer.display();
     this.rings.push(newLayer);
 
@@ -69,19 +77,41 @@ class Blob {
     }
   }
 
+  checkPath(direction, path) {
+    if (direction == "up") {
+      return (
+        (this.rings[this.currentRing].xCoordinates[this.keyIndex] ==
+          path.startRing.xCoordinates[path.index] ||
+          this.rings[this.currentRing].xCoordinates[this.keyIndex + 1] ==
+            path.startRing.xCoordinates[path.index] ||
+          this.rings[this.currentRing].xCoordinates[this.keyIndex - 1] ==
+            path.startRing.xCoordinates[path.index]) &&
+        this.currentRing == path.startRing.id - 1
+      );
+    } else if (direction == "down") {
+      return (
+        (this.rings[this.currentRing].xCoordinates[this.keyIndex] ==
+          path.endRing.xCoordinates[path.index] ||
+          this.rings[this.currentRing].xCoordinates[this.keyIndex + 1] ==
+            path.endRing.xCoordinates[path.index] ||
+          this.rings[this.currentRing].xCoordinates[this.keyIndex - 1] ==
+            path.endRing.xCoordinates[path.index]) &&
+        this.currentRing == path.endRing.id - 1
+      );
+    }
+  }
+
+  createGoals() {
+    console.log('create goal!');
+    this.goals.push(new Goal(1, 0, this, "humor"));
+  }
+
   draw() {
     push();
     translate(this.x, this.y);
-    fill(222, 239, 231, 15);
+    fill(222, 239, 231, 5);
     stroke(255);
     strokeWeight(1);
-
-    this.maxNoise = 2 + this.angryLevel * 6;
-    this.phaseRate = 0.007 + this.angryLevel * 0.007;
-    this.zoffRate = 0.01 + this.angryLevel * 0.01;
-    this.blobiness = 20 + this.angryLevel * 20;
-    this.offset = 0.01 + this.angryLevel * 10;
-
     this.phase += this.phaseRate;
     this.zoff += this.zoffRate;
 
@@ -96,6 +126,8 @@ class Blob {
     for (let i = 0; i < this.rings.length; i++) {
       this.rings[i].phase = this.phase;
       this.rings[i].zoff = this.zoff;
+      this.rings[i].blobiness = this.blobiness;
+      this.rings[i].maxNoise = this.maxNoise;
       this.rings[i].display();
     }
 
@@ -122,38 +154,43 @@ class Blob {
       }
     }
     if (keyIsDown(UP_ARROW)) {
-      console.log("Up Arrow Pressed")
-      for(var i=0;i< this.paths.length;i++){
-       
-        if(this.rings[this.currentRing].xCoordinates[this.keyIndex]==this.paths[i].startRing.xCoordinates[this.paths[i].index]&&this.currentRing==this.paths[i].startRing.id-1){
-          
-          this.currentRing = this.paths[i].endRing.id-1
-          break
+      console.log("Up Arrow Pressed");
+      for (var i = 0; i < this.paths.length; i++) {
+        if (this.checkPath("up", this.paths[i])) {
+          this.currentRing = this.paths[i].endRing.id - 1;
+          break;
         }
       }
-      
     }
     if (keyIsDown(DOWN_ARROW)) {
-       
-       console.log("Down Arrow Pressed")
-       for(var i=0;i< this.paths.length;i++){
-       
-        if(this.rings[this.currentRing].xCoordinates[this.keyIndex]==this.paths[i].endRing.xCoordinates[this.paths[i].index]&&this.currentRing==this.paths[i].endRing.id-1){
-          
-          this.currentRing = this.paths[i].startRing.id-1
-          break
+      console.log("Down Arrow Pressed");
+      for (var i = 0; i < this.paths.length; i++) {
+        if (this.checkPath("down", this.paths[i])) {
+          this.currentRing = this.paths[i].startRing.id - 1;
+          break;
         }
       }
     }
+
+    // Display player if scene > 0
     if (scene > 0) {
       this.player.display(this.rings[this.currentRing], this.keyIndex);
     }
+
+    for (var i = 0; i < this.paths.length; i++) {
+      if (
+        this.checkPath("up", this.paths[i]) ||
+        this.checkPath("down", this.paths[i])
+      ) {
+        this.paths[i].selected = true;
+        break;
+      } else {
+        this.paths[i].selected = false;
+      }
+    }
     pop();
   }
-
 }
-
-
 
 class Path {
   // idea: add offset +/- x and y coordinates to add wiggle to the crack
@@ -163,6 +200,8 @@ class Path {
     this.endRing = endRing;
     this.thickness = 3; // Add as input later
     this.index = index;
+    this.color = color(255);
+    this.selected = false;
   }
 
   display() {
@@ -178,24 +217,52 @@ class Path {
     vertex(startX, startY);
     vertex(endX, endY);
     endShape();
+
+    // Display start and end points of the path
+    noStroke();
+    fill(30);
+    if (this.selected === true) {
+      fill(200, 80, 20, 14);
+      for (let i = 0; i < 20; i++) {
+        ellipse(startX, startY, i * 3);
+        ellipse(endX, endY, i * 3);
+      }
+      fill(20);
+      ellipse(startX, startY, this.thickness * 3);
+      ellipse(endX, endY, this.thickness * 3);
+    } else {
+      fill(50);
+      ellipse(startX, startY, this.thickness * 3);
+      ellipse(endX, endY, this.thickness * 3);
+    }
     pop();
   }
 }
 
 class Ring {
-  constructor(id, radius, phase, zoff) {
+  constructor(id, radius, phase, zoff, blobiness, maxNoise, main = true) {
     this.id = id;
     this.radius = radius;
     this.xCoordinates = [];
     this.yCoordinates = [];
     this.angryLevel = 0;
-    this.maxNoise = 2 + this.angryLevel * 9;
+    this.maxNoise = maxNoise;
     this.phase = phase;
     this.phaseRate = 0.001;
     this.offset = 0.01;
     this.zoff = zoff;
     this.zoffRate = 0.01;
-    this.blobiness = 20;
+    this.blobiness = blobiness;
+    this.main = main;
+
+    if (this.main === true) {
+      this.minorRings = [];
+      for (let i = 0; i < ringsPerLvl; i++) {
+        this.minorRings.push(
+          new Ring(id, radius - 6 * i, phase, zoff, blobiness, maxNoise, false)
+        );
+      }
+    }
   }
 
   display() {
@@ -207,8 +274,11 @@ class Ring {
     this.yCoordinates = [];
     push();
     translate(this.x, this.y);
+    if (this.main === true) {
+      strokeWeight(5);
+    }
     beginShape();
-    for (var a = 0; a < TWO_PI; a += 0.1) {
+    for (var a = 0; a < TWO_PI; a += 0.05) {
       xoff = map(cos(a + this.phase), -1, 1, 0, this.maxNoise);
       yoff = map(sin(a + this.phase), -1, 1, 0, this.maxNoise);
       this.offset = map(
@@ -227,5 +297,53 @@ class Ring {
     }
     endShape(CLOSE);
     pop();
+
+    if (this.main === true) {
+      for (let i = 0; i < this.minorRings.length; i++) {
+        this.minorRings[i].phase = this.phase;
+        this.minorRings[i].zoff = this.zoff;
+        this.minorRings[i].blobiness = this.blobiness;
+        this.minorRings[i].maxNoise = this.maxNoise;
+        this.minorRings[i].display();
+      }
+    }
+  }
+}
+
+class Goal {
+  constructor(ringIndex, keyIndex, blob, att) {
+    this.blob = blob;
+    this.player = blob.player;
+    console.log('created goal with player: ' + this.player);
+    this.active = true;
+    this.ring = this.blob.rings[ringIndex];
+    this.position = keyIndex;
+    this.att = att;
+  }
+
+  capture() {
+    score += 1;
+    this.player.attributes[this.att] += 1;
+    this.active = false;
+  }
+
+  display() {
+    if (this.active) {
+      push();
+      translate(this.blob.x, this.blob.y);
+      noStroke();
+      fill(255);
+      ellipse(
+        this.ring.xCoordinates[keyIndex],
+        this.ring.yCoordinates[keyIndex],
+        5
+      );
+      fill(5, 5, 5, 12);
+      for (let i = 0; i < 40; i++) {
+        ellipse(this.ring.xCoordinates[keyIndex],
+          this.ring.yCoordinates[keyIndex], i * 2);
+      }
+      pop();
+    }
   }
 }
--- player.js
+++ player.js
@@ -1,24 +1,30 @@
 class Player {
   constructor(playerSize) {
     this.playerSize = playerSize;
-    this.humor = 0;
-    this.empathy = 0;
-    this.optimism = 0;
-    this.ambition = 0;
-    this.creativity = 0;
+    this.attributes = {
+      'humor': 0,
+      'empathy': 0,
+      'optimism': 0,
+      'ambition': 0,
+      'creativity': 0
+    }
     this.hp = 100;
+    console.log('created player');
   }
 
   display(ring, keyIndex) {
     push();
-    translate(this.x, this.y);
     noStroke();
-    fill("HotPink");
+    fill(255);
     ellipse(
       ring.xCoordinates[keyIndex],
       ring.yCoordinates[keyIndex],
       this.playerSize
     );
+    fill(255, 220, 200, 6);
+    for (let i = 0; i < 70; i++) {
+      ellipse(ring.xCoordinates[keyIndex], ring.yCoordinates[keyIndex], i * 3);
+    }
     pop();
   }
 }
