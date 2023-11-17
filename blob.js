9b74f0333b870770fb9e1dc6fc3efd640bce9595
Prashant Yella <prashantyella@Prashants-MacBook-Air.local>
2023-11-16 23:36:59 -0500

    Tone JS Basics

--- blob.js
+++ blob.js
@@ -29,6 +29,10 @@ class Blob {
     this.yCount = 0;
     this.keyIndex = 0;
     this.currentRing = 0;
+    this.healthMax = 100;
+    this.healthLvl = 100;
+    this.regenRate = 0.5; 
+
   }
 
   addPaths(n) {
@@ -147,13 +151,45 @@ class Blob {
     }
     if (scene > 0) {
       this.player.display(this.rings[this.currentRing], this.keyIndex);
+
+      //health bar
+      if(this.currentRing>0){
+        
+        this.regenRate = -this.currentRing*(0.05);
+      }
+      else{
+        this.regenRate = 0.1;
+      }
+      if(this.healthLvl>0){
+
+        push();
+        noFill();
+        stroke(255);
+        rect(500,-300,this.healthMax,20)
+        pop();
+        push()
+        noStroke()
+        fill(144,238,144)
+        this.healthLvl += this.regenRate;
+        this.healthLvl = min(this.healthMax,this.healthLvl)
+        if(this.healthLvl<20){
+          fill(216,34,41)
+          //this.regenRate = -this.currentRing*(0.001);
+        }
+        console.log("Health level:",this.healthLvl)
+        rect(500,-300,this.healthLvl,20)
+        pop()
+      }
+      else{
+        noLoop();
+        //stop or reset game here
+      }
+
     }
     pop();
   }
 
 }
-
-
 
 class Path {
   // idea: add offset +/- x and y coordinates to add wiggle to the crack
--- sketch.js
+++ sketch.js
@@ -13,11 +13,17 @@ let sliderH = 32;
 let buttonW = 100;
 let buttonH = 40;
 let lvl = 1;
+let maxLvl = 5;
 let attributes;
 let scene = 0;
 let mountain;
 let questions;
 let buttons = [];
+let introAmbiences;
+let currentAmbience;
+let characterSoundtracks = [];
+let mainSoundtrack;
+
 
 function loadAttributes() {
   attributes = Object.keys(questions).length;
@@ -25,9 +31,24 @@ function loadAttributes() {
 
 function preload() {
   questions = loadJSON("assets/questions.json", (callback = loadAttributes));
+  //loading all the ambient sounds in this array
+  introAmbiences = new Tone.Players({
+    amb1 : "./assets/sounds/introAmbience/1.mp3", amb2 : "./assets/sounds/introAmbience/2.mp3",
+    amb3 : "./assets/sounds/introAmbience/3.mp3", amb4 : "./assets/sounds/introAmbience/4.mp3",
+    amb5: "./assets/sounds/introAmbience/5.mp3"
+  }).toDestination();
+
+  currentAmbience = new Tone.Player("./assets/sounds/introAmbience/1.mp3").toDestination();
+  currentAmbience.autostart = true;
+
+  //loading main soundtrack
+  mainSoundtrack = new Tone.Player("./assets/sounds/soundtracks/mainSoundtrack.mp3").toDestination();
+  mainSoundtrack.autostart = true;
+
 }
 
 function setup() {
+  
   createCanvas(windowWidth, windowHeight);
   mountain = new Mountain(0);
   gui = createGui();
@@ -38,7 +59,6 @@ function setup() {
 
 function draw() {
   background(0);
-  //mountain.display();
   stroke(255);
   fill(255, 70);
   textSize(34);
@@ -49,13 +69,19 @@ function draw() {
   blob.draw();
 
   if (scene == 0) {
+    
+    
     characterCreation();
   }
 
   // Main stage
-
+  if(scene == 1){
+    
+    
+  }
   // Isometric stage
   if (scene == 2) {
+    
     blob.x = windowWidth / 4;
     blob.y = windowHeight / 2;
   }
